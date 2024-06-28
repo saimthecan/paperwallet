59174ddce293d2b0578a3de653b916eb6195ea7d
saimthecan <sco11@protonmail.com>
2024-06-28 23:36:37 +0300

    update

--- src/components/PrivateKey.js
+++ src/components/PrivateKey.js
@@ -1,11 +1,45 @@
 import React from "react";
-import { Container } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Text,
+  Heading,
+  VStack
+} from "@chakra-ui/react";
 
 const PrivateKey = () => {
+  const privateKey = process.env.REACT_APP_PRIVATE_KEY; // Retrieve private key from .env file
+
   return (
-    <Container maxW="container.xl" mt={{ base: "-1.5rem", md: "0" }}>
-      <h1>Private Key Page</h1>
-      <p>This is a protected route. Only accessible if logged in.</p>
+    <Container maxW="container.md" mt={{ base: "2rem", md: "4rem" }} p={4}>
+      <Box
+        p={6}
+        boxShadow="lg"
+        rounded="lg"
+        bg="white"
+        textAlign="center"
+      >
+        <Heading as="h1" size="xl" mb={4}>
+          Private Key
+        </Heading>
+        <Box
+          p={4}
+          borderWidth={1}
+          borderRadius="lg"
+          bg="gray.50"
+          color="gray.700"
+          fontFamily="monospace"
+          fontSize="lg"
+          mb={6}
+        >
+          {privateKey}
+        </Box>
+        <VStack spacing={4}>
+          <Text fontSize="2xl" color="teal.500">
+            Birlikte geçireceğiniz her gün mutlulukla dolu olsun
+          </Text>
+        </VStack>
+      </Box>
     </Container>
   );
 };
